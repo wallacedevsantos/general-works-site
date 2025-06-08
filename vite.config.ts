993898fddcf30433f6ec47719738aa6247b416ee
wallacedevsantos <wallacedev.oliveirasantos@gmail.com>
2025-06-08 17:11:09 -0300

    subindo dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    base: mode === 'production' ? '/general-works-site/' : '/',
     host: "::",
     port: 8080,
   },
