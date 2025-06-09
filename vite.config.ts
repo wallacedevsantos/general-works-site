917c0dd1f7ba881f684f2c7f2eb0adfe88c8c56e
wallacedevsantos <wallacedev.oliveirasantos@gmail.com>
2025-06-09 07:45:55 -0300

    atualização do vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/general-works-site/' : '/', // <- base na raiz, não dentro de server
   server: {
-    base: mode === 'production' ? '/general-works-site/' : '/',
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
